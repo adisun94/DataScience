2b6ec5de243a8142a88ecfdea7fafe1b4a18c0b7
Aditya Sundar <adisun@DESKTOP-9F8NM5D.localdomain>
2022-11-22 21:34:15 -0600

    web app using Dash

---
+++ dashboard/index.py
@@ -0,0 +1,31 @@
+from dash import  dcc, html
+from dash.dependencies import Input, Output
+
+# Connect to main app.py file
+from app import app
+from app import server
+
+# Connect to your app pages
+from apps import screen
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div([
+        dcc.Link('Model          |      ', href='/apps/screen'),
+        ], className="row"),
+    html.Div(id='page-content', children=[])
+    ])
+
+
+@app.callback(Output('page-content', 'children'),
+        [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/screen':
+        return screen.layout
+    else:
+        return "Please choose a link"
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
